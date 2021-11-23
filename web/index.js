d4ee5480d1c60b70b114c0f168f58176bb0ea6ce
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-11-23 13:56:37 +0100

    updated js and arduino

--- web/index.js
+++ web/index.js
@@ -1,5 +1,7 @@
 let websock;
-let auth = false;
+let isAuth = false;
+let isLogin = false;
+let cred_list = [];
 function connectWS() {
     let wsUri = `ws://${document.getElementById('ip').value}/ws`;
     console.log(wsUri);
@@ -18,19 +20,94 @@ function authorize(pass) {
     }
     websock.send(JSON.stringify(data));
 }
+
+function login(pass) {
+    let data = {
+        command: "login",
+        pass: "ovuvog"
+    }
+    websock.send(JSON.stringify(data));
+}
+
+function list() {
+    let data = {
+        command: "list"
+    }
+    websock.send(JSON.stringify(data));
+}
+
+function get_credential(dir) {
+    let data = {
+        command: "get_credential",
+        dir
+    }
+    websock.send(JSON.stringify(data));
+}
+
+function reload() {
+    let data = {
+        command: "reload"
+    }
+    websock.send(JSON.stringify(data));
+}
+
+function add_credential(email, username, password, website) {
+    let data = {
+        command: "add_credential",
+        email,
+        username,
+        password,
+        website
+    }
+    websock.send(JSON.stringify(data));
+}
+function add_credential_without_password(email, username, website) {
+    let data = {
+        command: "add_credential_gen_password",
+        email,
+        username,
+        website
+    }
+    websock.send(JSON.stringify(data));
+}
 function socketMessageListener(evt) {
     let data = JSON.parse(evt.data)
-    console.log(data);
     switch (data.resultof) {
         case "auth":
             if (data.state = "success") {
                 console.log("Aithed Successfully!");
-                auth = true;
-                window.location.href="login.html";
+                isAuth = true;
+                // window.location.href = "login.html";
             }
             break;
-
+        case "login":
+            if (data.state = "success") {
+                console.log("Login Successfully!");
+                isLogin = true;
+            }
+            break;
+        case "list":
+            if (data.state = "success") {
+                console.log("List Successfully!");
+                cred_list = data.credentials;
+                console.log(cred_list);
+            }
+            break;
+        case "add_credential":
+            if (data.state = "success") {
+                console.log("Added Successfully!");
+                reload();
+                list();
+            }
+            break;
+        case "get_credential":
+            if (data.state = "success") {
+                console.log("Added Successfully!");
+                console.log(data)
+            }
+            break;
         default:
+            console.log(evt);
             break;
     }
 }
